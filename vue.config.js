bdaa04e79326d8d4bb06e53a7ae2a713f04a0c50
Kirill Voronin <yoursdearboy@gmail.com>
2022-09-28 10:29:10 +0000

    Added FHIR proxy to devserver

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 
 const rawArgv = process.argv.slice(2)
-const args = require('minimist')(rawArgv);
-const port = args.port || process.env.PORT;
+const args = require('minimist')(rawArgv)
+const port = args.port || process.env.PORT
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -11,9 +11,17 @@ module.exports = defineConfig({
       libraryTarget: 'system'
     },
     devServer: {
+      // port to make HMR and Live Reload work with single-spa
       client: {
         webSocketURL: {
           port
+        }
+      },
+      // /fhir proxy
+      proxy: {
+        '/fhir': {
+          target: process.env.FHIR_TARGET,
+          pathRewrite: { '^/fhir': process.env.FHIR_PATH }
         }
       }
     }
