3565faa9ee2088f52f1407bdb39d1151f42e5311
Kirill Voronin <yoursdearboy@gmail.com>
2022-09-28 09:08:25 +0000

    Fixed devserver websocket port issue

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const rawArgv = process.argv.slice(2)
+const args = require('minimist')(rawArgv);
+const port = args.port || process.env.PORT;
+
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     output: {
       libraryTarget: 'system'
+    },
+    devServer: {
+      client: {
+        webSocketURL: {
+          port
+        }
+      }
     }
-  },
-  devServer: {
-    host: 'localhost'
   }
 })
