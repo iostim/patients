87c9d81d4511e0b05873ae8c2eb22f181d06673b
Kirill Voronin <yoursdearboy@gmail.com>
2022-09-28 12:50:01 +0000

    Moved FHIR proxy to separate module

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const proxyFHIR = require("webpack-proxy-fhir")
 
 const rawArgv = process.argv.slice(2)
 const args = require('minimist')(rawArgv)
 const port = args.port || process.env.PORT
 
+
 // Proxy
 const proxy = {}
 
 // FHIR proxy on /fhir
-if (process.env.FHIR_TARGET) {
-  proxy['fhir'] = {
-    target: process.env.FHIR_TARGET
-  }
-  if (process.env.FHIR_PATH) {
-    proxy['fhir']['pathRewrite'] = {
-      '^/fhir': process.env.FHIR_PATH
-    }
-  }
+if (process.env.FHIR_PROXY_URL) {
+  Object.assign(proxy, proxyFHIR(process.env.FHIR_PROXY_URL))
 }
 
 module.exports = defineConfig({
